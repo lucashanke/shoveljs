0143e65f195dc62e7314268bb8b97d175c23a1b4
gleissonmattos <contato.mattos@outlook.com>
2023-03-22 14:25:44 -0300

    feat: add option to delay and limit amount of messages processed

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,11 @@ const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
 const amqplib = require('amqplib');
 
+const setDelay = (time) => {
+  console.log('waiting for break...');
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
 // eslint-disable-next-line no-unused-expressions
 yargs(hideBin(process.argv))
   .command(
@@ -16,9 +21,12 @@ yargs(hideBin(process.argv))
       .option('h', { alias: 'host', describe: 'RabbitMQ host', type: 'string', default: 'localhost' })
       .option('P', { alias: 'port', describe: 'RabbitMQ port', type: 'number', default: 5672 })
       .option('u', { alias: 'user', describe: 'RabbitMQ user', type: 'string', demandOption: true })
-      .option('p', { alias: 'password', describe: 'RabbitMQ user', type: 'string', demandOption: true }),
+      .option('p', { alias: 'password', describe: 'RabbitMQ user', type: 'string', demandOption: true })
+      .option('d', { alias: 'delay', describe: 'Delay interval between messages (milliseconds)', type: 'number' })
+      .option('l', { alias: 'limit', describe: 'Limit of messages to be processed', type: 'number' }),
+
     async (argv) => {
-      const { host, port, user, password, ssl, originQueue, destinationQueue } = argv;
+      const { host, port, user, password, ssl, originQueue, destinationQueue, delay, limit } = argv;
       const conn = await amqplib.connect(
         `amqp${ssl ? 's' : ''}://${user}:${password}@${host}:${port}`,
       );
@@ -34,21 +42,32 @@ yargs(hideBin(process.argv))
       if (messageCount === 0) {
         console.log('No messages to shovel.');
       } else {
-        await new Promise((resolve) => {
-          channel.consume(originQueue, (msg) => {
+        await new Promise(async (resolve) => {
+          const perform = async () => {
+            if (counter === limit) {
+              resolve();
+              return;
+            }
+
+            const msg = await channel.get(originQueue);
+
             if (msg !== null) {
               channel.ack(msg);
               channel.sendToQueue(destinationQueue, msg.content, msg.properties,
-                (err, ok) => {
+                async (err, ok) => {
                   counter += 1;
                   if (counter === messageCount) resolve();
+                  delay && await setDelay(delay);
+                  perform();
                 },
               );
             } else {
               console.error('Consumer channel was closed.');
               resolve();
             }
-          });
+          }
+
+          perform();
         });
         console.log(
           `${counter} messages shoveled from ${originQueue} to ${destinationQueue}`,
